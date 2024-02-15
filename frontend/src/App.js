2b6d0c4339ba1b27baac49c67ef0f3fcbec3e8b7
Aaron <125625511+atburn@users.noreply.github.com>
2024-02-15 14:03:19 -0800

    Restructured to have a backend and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home';
 import Header from './components/Header';
+import axios from 'axios';
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <button onClick={testBackend}>Test</button>
+
     </>
   );
 }
 export default App;
+
+async function testBackend() {
+  await axios.get("http://localhost:2000/");
+}
