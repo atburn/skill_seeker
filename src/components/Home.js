5957875c10b1eb312b3b1709906faab966954dce
tuphan17 <phandinhtu173@gmail.com>
2024-02-15 12:26:57 -0800

    add to home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import { Link } from 'react-router-dom'; 
 
-function Home() {
+const jobData =  [
+  {id: 1, title: 'Software Developer'}, 
+  {id: 2, title: 'Frontend Engineer'}
+];
+
+function Grid({children}) {
   return (
-    <div>
-      <Header title="Skill Seeker" />
-      {/*  Home page content */}
+    <div className="grid">
+      {children}
     </div>
   );
 }
 
-export default Home;
+function Home() {
+
+  return (
+    <Grid>
+      <header>
+        <div className="header-title">  
+          Skill Checker
+        </div>
+        <nav>
+          <Link to="/profile">Profile</Link> 
+        </nav>
+      </header>
+
+      <main>
+        <section>
+          <h2>Latest Jobs</h2>
+          {
+            jobData.map(job => (
+              <div key={job.id}>{job.title}</div> 
+            ))
+          }
+        </section>  
+
+        <section>
+         <h2>Top Companies</h2> 
+        </section>
+      </main>
+    </Grid>
+  );
+}
+  
+ export default Home;
