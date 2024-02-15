8f1483a85e1e3c7a6ca254aa60ba568d041a2a98
Aaron <125625511+atburn@users.noreply.github.com>
2024-02-14 18:27:12 -0800

    Added webservices, worked a bit on the database integration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './Home';
-import Login from './Login';
-import SignUp from './SignUp';
-import Profile from './Profile';
-import JobPosting from './JobPosting';
-import Employer from './Employer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Profile from './components/Profile';
+import JobPosting from './components/JobPosting';
+import Employer from './components/Employer';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/job-posting" component={JobPosting} />
-        <Route path="/employer" component={Employer} />
-        <Route path="/" component={Home} />
-      </Switch>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/job-posting" element={<JobPosting />} />
+        <Route path="/employer" element={<Employer />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/database/DatabaseHandler.js
@@ -0,0 +1,34 @@
+const DATABASE_URL = "https://skill-seeker-460-default-rtdb.firebaseio.com/";
+
+export default class DatabaseHandler {
+    constructor() {
+
+    }
+
+    static getUserById(userId) {
+        return null;
+    }
+
+    static getUsersByQuery(query) {
+        return [];
+    }
+
+    static createNewJobPosting(posting) {
+
+    }
+
+    static updatePosting(posting) {
+
+    }
+
+    static getPostingById(postingId) {
+        return null;
+    }
+
+    static getPostingsByQuery(query) {
+        return [];
+    }
+
+}
+
+
