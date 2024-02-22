5c2d06d77f5ccf062cda30413b7487afa2ab4b19
Aaron <atburn@uw.edu>
2024-02-21 19:36:45 -0800

    Update FirebaseDatabaseHandler.js

--- backend/FirebaseDatabaseHandler.js
+++ backend/FirebaseDatabaseHandler.js
@@ -2,7 +2,7 @@ import { getDatabase, ref, onValue } from "firebase/database";
 
 // const API_URL = "https://skill-seeker-460-default-rtdb.firebaseio.com/";
 
-
+// https://firebase.google.com/docs/database/web/read-and-write
 export default class FirebaseDatabaseHandler {
 
     constructor() {
