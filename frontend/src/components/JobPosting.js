7093edf63f58cfaa781b978006edb85449ef8c22
Ayemyat Oo <ayemyatmo.oo@gmail.com>
2024-03-04 12:23:11 -0800

    Update JobPosting.js and fix JobPosting.css

--- frontend/src/components/JobPosting.js
+++ frontend/src/components/JobPosting.js
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react';
-import './JobPosting.css'; 
+import './JobPosting.css';
 import axios from 'axios';
 
 function JobPosting() {
     const [searchTerm, setSearchTerm] = useState('');
     const [jobs, setJobs] = useState({});
+    const [companies, setCompanies] = useState({});
 
     useEffect(() => {
         fetchJobs();
-    }, []); 
+        fetchCompanies();
+    }, []);
 
     const fetchJobs = async () => {
         try {
@@ -19,6 +21,19 @@ function JobPosting() {
         }
     };
 
+    const fetchCompanies = async () => {
+        try {
+            const response = await axios.get('http://localhost:2000/companies');
+            const companiesById = response.data.reduce((obj, company) => {
+                obj[company.uid] = company.name; // Store company name by its ID
+                return obj;
+            }, {});
+            setCompanies(companiesById);
+        } catch (error) {
+            console.error('Error fetching companies:', error);
+        }
+    };
+
     const handleSearch = (event) => {
         setSearchTerm(event.target.value);
     }
@@ -26,7 +41,6 @@ function JobPosting() {
     const filteredJobs = Object.keys(jobs).reduce((filtered, companyId) => {
         const companyJobs = jobs[companyId];
 
-       
         const filteredCompanyJobs = Object.keys(companyJobs).filter(jobId =>
             companyJobs[jobId].title.toLowerCase().includes(searchTerm.toLowerCase()) ||
             companyJobs[jobId].location.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -36,7 +50,6 @@ function JobPosting() {
             return obj;
         }, {});
 
-       
         if (Object.keys(filteredCompanyJobs).length > 0) {
             filtered[companyId] = filteredCompanyJobs;
         }
@@ -58,7 +71,7 @@ function JobPosting() {
             <div className="job-posting-results">
                 {Object.keys(filteredJobs).map(companyId => (
                     <div key={companyId} className="company-jobs">
-                        <h2>Company ID: {companyId}</h2>
+                        <h2>Company: {companies[companyId] || 'Unknown'}</h2> {/* Display company name */}
                         {Object.keys(filteredJobs[companyId]).map(jobId => (
                             <div key={jobId} className="job-posting">
                                 <h3>{filteredJobs[companyId][jobId].title}</h3>
@@ -74,3 +87,4 @@ function JobPosting() {
 }
 
 export default JobPosting;
+
