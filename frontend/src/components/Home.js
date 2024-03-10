50c0a7f79f199914cfc8b38feffe70628f4f3f21
Tu <phandinhtu173@gmail.com>
2024-03-09 22:21:06 -0800

    Update Home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,37 +1,27 @@
 import { Link } from 'react-router-dom'; 
 import backgroundImage from './background.jpg';
 import logoImage from './logo.png';
-function Home() {
 
+function Home() {
   return (
     <div className="home" style={{backgroundImage: `url(${backgroundImage})`}}>
-     <div className="logo">
-        <img src={logoImage} alt="Site Logo" />
+      <div className="overlay"></div>
+      <div className="home-content">
+        <div className="logo">
+          <img src={logoImage} alt="Site Logo" />
+        </div>
+        <div className="tagline">
+          <h1>Find the Right Job in the Right Company</h1>
+          <p>A Place for <span className="highlight">Students</span> to Find Latest Jobs in USA</p>
+        </div>
+        <div className="cta">
+          <Link to="/jobs">
+            <button className="btn">Browse Jobs</button> 
+          </Link>
+        </div>
       </div>
-
-      <div className="tagline">
-      <h3>A Place for <span className="underline">Students</span> to Find Latest Jobs in USA</h3>
-        <p>
-          Latest New Grads/Internships for UW Students  
-        </p>
-      </div>
-
-
-
-      <div className="quote">
-        <h1 style={{color: '#0077ff'}}>
-           Find the Right Job in the Right Company
-        </h1>
-        
-        <Link to="/jobs">
-           <button className="btn">Browse Jobs</button> 
-        </Link>
-
-      </div>
-
     </div>
   );
-
 }
 
 export default Home;
